e107ffd0af54e744621bfc0c8bec6b5bc010a864
Adarsh Sharma <addy120323@gmail.com>
2024-01-23 00:15:49 +0530

    Weather app

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "68727caf55b6ef09aac56a28b2fbf726",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
